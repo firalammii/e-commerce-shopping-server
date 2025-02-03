40d22f89145445290bc8281e913c2d0eef1994f0
firalammii <90164296+firalammii@users.noreply.github.com>
2025-02-03 13:03:46 +0300

    corsop

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"https://firashoppingapp.onrender.com",
+	"https://firashopping.netlify.app"
 ]
 
 const corsOptions = {
